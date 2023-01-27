04f0f7638fe5904d9a40566de6a5a52d52be0623
BrunodevBandeira <brunodev29ab@gmail.com>
2023-01-27 11:49:38 -0300

    Add endpointGet

--- src/api/EndpointGet.js
+++ src/api/EndpointGet.js
@@ -1,26 +1,46 @@
-import React from 'react';;
+import React from 'react';
 
 const EndpointGet = () => {
 
     const [id, setId] = React.useState("");
-    const [dog, setDog] = React.useState(null);
+    const [dog, setDog] = React.useState([]);
     
-//https://api.thedogapi.com/v1/breeds/
+// const url = "https://api.thedogapi.com/v1/breeds/15";
 
     React.useEffect(() => {
-        fetch('https://api.thedogapi.com/v1/breeds/15')
-        .then((r) => r.json())
-        .then((json) =>  console.log(json))
-    }, []); 
+        const fetchDogData = async () => {
+            try {
+              const res = await fetch("https://api.thedogapi.com/v1/breeds")
+              const data = await res.json()
+              setDog(data)
+            } catch (error) {
+              console.error(error)
+            }
+          }
+    
+          fetchDogData();
+  }, [])
 
-    return (
-        <div>
-            {/* {dog.map(({name}) => (
-                // <img src={foto.image}   />
-            ))}   */}
-            <p> {dog.name} </p>
-        </div>
-      );
+  return (
+    <>
+
+      <section >
+        {dog.map((item) => (
+          <div key={item.id}>
+            <article>
+              <h1 >
+                {item.name}
+              </h1>
+              <p>
+                <img src={`https://cdn2.thedogapi.com/images/${item.reference_image_id}.jpg`}
+                alt={item.name} title={item.name} />
+              </p>
+            </article>
+          </div>
+        ))}
+      </section>
+    </>
+  )
 }
 
-export default EndpointGet
+export default EndpointGet;
